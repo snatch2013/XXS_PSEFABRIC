dec0c0f701ffe60905060035ade17e5b046b1388
snatch2013 <blackabdula@gmail.com>
2018-10-30 11:10:57 +0100

    Update cidr_to_netmask.py - use ipaddress library
    Using ipaddress we can write more clear code.

--- PSEF_SCRIPTS/cidr_to_netmask.py
+++ PSEF_SCRIPTS/cidr_to_netmask.py
@@ -1,10 +1,5 @@
-import socket
-import struct
+import ipaddress
 
 def cidr_to_netmask(cidr):
-    network, net_bits = cidr.split('/')
-    host_bits = 32 - int(net_bits)
-    netmask = socket.inet_ntoa(struct.pack('!I', (1 << 32) - (1 << host_bits)))
-    b1,b2,b3,b4 = network.split('.')
-    gate = str(b1) + '.' + str(b2) + '.' + str(b3) + '.' + str(int(b4) + 1)
-    return network, netmask, gate
+    ip = ipaddress.ip_network(cidr)
+    return str(ip.network_address), str(ip.netmask), str(ip.network_address + 1)
